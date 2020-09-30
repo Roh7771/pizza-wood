cd60515e8776794639f8b0d3124afc4a487bf3e1
Belov Roman <belov.rom-77@yandex.ru>
2020-09-30 18:01:32 +0300

    Добавляет еще больше элементов

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './style/main.sass';
+
+document.getElementById("myinput").oninput = function() {
+  this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
+};
