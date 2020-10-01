d223e052ed65a931b533a27fb02673f6958fc663
Roman Belov <belov.rom-77@yandex.ru>
2020-10-01 08:28:04 +0300

    Доделывает основную верстку

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './style/main.sass';
-
-document.getElementById("myinput").oninput = function() {
-  this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
-};
+import './js/slider';
+import './js/check-range';
+import './js/month-range';
